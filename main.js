9b0ce35bb7901b090299be9b25d266cdb70e8df5
Adam Donaghy <donaghyam@gmail.com>
2022-01-13 11:57:31 -0600

    working

---
+++ main.js
@@ -0,0 +1,24 @@
+const computer = {
+	id: 1,
+	manufacturer: "Apple",
+	make: "Macbook",
+	model: "Pro",
+	specs: {
+		memory: 32,
+		hardDrive: "500 GB",
+		processor: 2.4
+	}
+}
+
+const htmlOutput = (computerObject) => {
+    const computerHtml = `<section id= computer--${computerObject.id}">
+	<h1>${computerObject.manufacturer} ${computerObject.make}</h1>
+	<div>Model: ${computerObject.model}</div>
+	<div>Memory: ${computerObject.specs.memory} GB</div>
+	<div>Hard drive space: ${computerObject.specs.hardDrive}</div>
+	<div>Processor speed: ${computerObject.specs.processor} Ghz</div>
+</section>`
+    return computerHtml
+}
+
+console.log(htmlOutput(computer))
